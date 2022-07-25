9e18c9dce46d2ffa513ba2e5112bf48f0f97224f
CatWithPaws <valentunkulakov6@gmail.com>
2022-07-25 19:07:45 +0300

    Inserted a few platforms to test

--- Assets/Scripts/Test/PlanetTest.cs
+++ Assets/Scripts/Test/PlanetTest.cs
@@ -377,6 +377,13 @@ namespace Planet.Unity
             var camera = Camera.main;
             Vector3 lookAtPosition = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, camera.nearClipPlane));
 
+            tileMap.SetFrontTile(4, 15, TileID.Platform);
+            tileMap.SetFrontTile(5, 15, TileID.Platform);
+            tileMap.SetFrontTile(6, 15, TileID.Platform);
+            tileMap.SetFrontTile(7, 15, TileID.Platform);
+            tileMap.SetFrontTile(8, 15, TileID.Platform);
+
+
             //tileMap.UpdateBackTileMapPositions((int)lookAtPosition.x, (int)lookAtPosition.y);
             //tileMap.UpdateMidTileMapPositions((int)lookAtPosition.x, (int)lookAtPosition.y);
             //tileMap.UpdateFrontTileMapPositions((int)lookAtPosition.x, (int)lookAtPosition.y);
