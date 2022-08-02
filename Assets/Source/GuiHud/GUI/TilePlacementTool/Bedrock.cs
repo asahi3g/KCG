aad8287ffa240cebf50867cda6a19f2339769e01
Choi <ridvansu@hotmail.com>
2022-08-02 15:00:56 +0300

    Build Tool Comments Added
    Build Tool Comments Added. resolves #468
    closes #468

--- Assets/Source/GuiHud/GUI/TilePlacementTool/Bedrock.cs
+++ Assets/Source/GuiHud/GUI/TilePlacementTool/Bedrock.cs
@@ -9,11 +9,14 @@ namespace KGUI.Tiles
 {
     public class Bedrock : GUIManager
     {
+        // Image's
         private Image BedrockTile;
         private Image Background;
 
+        // Planet
         private Planet.PlanetState _planet;
 
+        // Item Component Elements
         private int toolBarID;
         private InventoryEntity Inventory;
         private int selectedSlot;
@@ -21,8 +24,10 @@ namespace KGUI.Tiles
 
         public override void Initialize(Planet.PlanetState planet, AgentEntity agentEntity)
         {
+            // Planet Assign
             _planet = planet;
 
+            // Create Background
             int width = 16;
             int height = 16;
 
@@ -32,6 +37,7 @@ namespace KGUI.Tiles
             Background.SetScale(new Vector3(0.7f, 0.7f, 0.7f));
             Background.SetImageColor(Color.yellow);
 
+            // Create Tile
             BedrockTile = new Image("BedrockTile", Background.GetTransform(), width, height, "Assets\\StreamingAssets\\Tiles\\Blocks\\Bedrock\\bedrock.png");
             BedrockTile.SetPosition(new Vector3(0.0f, 0.0f, 0.0f));
             BedrockTile.SetScale(new Vector3(0.8f, 0.8f, 0.8f));
@@ -39,41 +45,51 @@ namespace KGUI.Tiles
 
         public override void Update(AgentEntity agentEntity)
         {
+            // Set Object Position
             ObjectPosition = new KMath.Vec2f(Background.GetTransform().position.x, Background.GetTransform().position.y);
 
+            // Set Inventory Element
             toolBarID = agentEntity.agentToolBar.ToolBarID;
             Inventory = _planet.EntitasContext.inventory.GetEntityWithInventoryID(toolBarID);
             selectedSlot = Inventory.inventorySlots.Selected;
 
+            // Create Item
             item = GameState.InventoryManager.GetItemInSlot(_planet.EntitasContext.itemInventory, toolBarID, selectedSlot);
             if (item.itemType.Type == Enums.ItemType.PlacementTool)
             {
+                // Activate Background
                 Background.GetGameObject().SetActive(true);
 
                 if (item.itemCastData.data.TileID == TileID.Bedrock)
                 {
+                    // Set Red After Selected
                     Background.SetImageColor(Color.red);
                 }
                 else
                 {
+                    // Set Yellow After Unselected
                     Background.SetImageColor(Color.yellow);
                 }
             }
             else
             {
+                // Deactivate Background
                 Background.GetGameObject().SetActive(false);
             }
         }
 
         public override void OnMouseClick(AgentEntity agentEntity)
         {
+            // Set Inventory Elements
             toolBarID = agentEntity.agentToolBar.ToolBarID;
             Inventory = _planet.EntitasContext.inventory.GetEntityWithInventoryID(toolBarID);
             selectedSlot = Inventory.inventorySlots.Selected;
 
+            // Create Item
             item = GameState.InventoryManager.GetItemInSlot(_planet.EntitasContext.itemInventory, toolBarID, selectedSlot);
             if (item.itemType.Type == Enums.ItemType.PlacementTool)
             {
+                // Set Data Tile ID to Pipe
                 item.itemCastData.data.TileID = TileID.Bedrock;
             }
         }
--- Assets/Source/GuiHud/GUI/TilePlacementTool/Dirt.cs
+++ Assets/Source/GuiHud/GUI/TilePlacementTool/Dirt.cs
@@ -9,11 +9,14 @@ namespace KGUI.Tiles
 {
     public class Dirt : GUIManager
     {
+        // Image's
         private Image DirtTile;
         private Image Background;
 
+        // Planet
         private Planet.PlanetState _planet;
 
+        // Item Component Elements
         private int toolBarID;
         private InventoryEntity Inventory;
         private int selectedSlot;
@@ -21,14 +24,17 @@ namespace KGUI.Tiles
 
         public override void Initialize(Planet.PlanetState planet, AgentEntity agentEntity)
         {
+            // Planet Assign
             _planet = planet;
 
+            // Create Background
             Background = new Image("DirtBackground", GameObject.Find("Canvas").transform, UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/Background.psd"));
             Background.SetImageType(UnityEngine.UI.Image.Type.Tiled);
             Background.SetPosition(new Vector3(-200.0f, -80.2f, 0));
             Background.SetScale(new Vector3(0.7f, 0.7f, 0.7f));
             Background.SetImageColor(Color.yellow);
 
+            // Create Tile
             int width = 16;
             int height = 16;
             DirtTile = new Image("DirtTile", Background.GetTransform(), width, height, "Assets\\StreamingAssets\\Tiles\\Blocks\\Dirt\\dirt.png");
@@ -38,40 +44,50 @@ namespace KGUI.Tiles
 
         public override void Update(AgentEntity agentEntity)
         {
+            // Set Object Position
             ObjectPosition = new KMath.Vec2f(Background.GetTransform().position.x, Background.GetTransform().position.y);
 
+            // Set Inventory Element
             toolBarID = agentEntity.agentToolBar.ToolBarID;
             Inventory = _planet.EntitasContext.inventory.GetEntityWithInventoryID(toolBarID);
             selectedSlot = Inventory.inventorySlots.Selected;
 
+            // Create Item
             item = GameState.InventoryManager.GetItemInSlot(_planet.EntitasContext.itemInventory, toolBarID, selectedSlot);
             if (item.itemType.Type == Enums.ItemType.PlacementTool)
             {
+                // Activate Background
                 Background.GetGameObject().SetActive(true);
                 if(item.itemCastData.data.TileID == TileID.Moon)
                 {
+                    // Set Red After Selected
                     Background.SetImageColor(Color.red);
                 }
                 else
                 {
+                    // Set Yellow After Unselected
                     Background.SetImageColor(Color.yellow);
                 }
             }
             else
             {
+                // Deactivate Background
                 Background.GetGameObject().SetActive(false);
             }
         }
 
         public override void OnMouseClick(AgentEntity agentEntity)
         {
+            // Set Inventory Elements
             toolBarID = agentEntity.agentToolBar.ToolBarID;
             Inventory = _planet.EntitasContext.inventory.GetEntityWithInventoryID(toolBarID);
             selectedSlot = Inventory.inventorySlots.Selected;
 
+            // Create Item
             item = GameState.InventoryManager.GetItemInSlot(_planet.EntitasContext.itemInventory, toolBarID, selectedSlot);
             if (item.itemType.Type == Enums.ItemType.PlacementTool)
             {
+                // Set Data Tile ID to Pipe
                 item.itemCastData.data.TileID = TileID.Moon;
             }
         }
