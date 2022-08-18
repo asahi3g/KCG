1d9266654d086d6176ad8196b602a93396f94ece
anser-software <ansersoftware@gmail.com>
2022-08-18 17:23:15 +0300

    Admin tool for placing geometry blocks on planet scene created; TODO: make it a proper selectable tool

---
+++ Assets/Source/PlanetTileMap/GeometryBlockPlacementTool.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using Enums.Tile;
+using KMath;
+using Item;
+using Animancer;
+using HUD;
+using PlanetTileMap;
+using Mech;
+using System.Linq;
+using TGen;
+
+namespace Planet
+{
+    public class GeometryBlockPlacementTool
+    {
+
+        private bool enableTileGrid = true;
+
+        private bool drawMapBorder = true;
+
+        private ForegroundPlacementTool placementTool;
+
+        public GeometryBlockPlacementTool(bool tileGrid, bool mapBorder)
+        {
+            enableTileGrid = tileGrid;
+            drawMapBorder = mapBorder;
+        }
+
+        public void UpdateToolGrid()
+        {
+            placementTool.UpdateToolGrid();
+        }
+
+        public void Initialize(ref PlanetState Planet, Material Material, Transform transform)
+        {
+            // Generating the map
+            Vec2i mapSize = new Vec2i(32, 32);
+
+            Planet.InitializeTGen(Material, transform);
+
+            GameState.TGenGrid.InitStage1(mapSize);
+
+            if (enableTileGrid)
+                GameState.TGenRenderGridOverlay.Initialize(Material, transform, mapSize.X, mapSize.Y, 30);
+
+            if (drawMapBorder)
+                GameState.TGenRenderMapBorder.Initialize(Material, transform, mapSize.X - 1, mapSize.Y - 1, 31);
+
+            placementTool = new ForegroundPlacementTool();
+            placementTool.Initialize(ref Planet);
+        }
+    }
+}
