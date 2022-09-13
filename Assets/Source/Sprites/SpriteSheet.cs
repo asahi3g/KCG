958d5c414d4fc713b6f13265497da8c729e7d1d5
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2022-09-13 11:54:25 +0100

    Update SpriteSheet
    comments

--- Assets/Source/Sprites/SpriteSheet.cs
+++ Assets/Source/Sprites/SpriteSheet.cs
@@ -2,14 +2,14 @@ namespace Sprites
 {
     public struct SpriteSheet
     {
-        public byte[] Data;
+        public byte[] Data; // RGBA8 array
 
-        public int Index;
+        public int Index; // Index of the sprite Sheet
         
-        public int SpriteWidth;
-        public int SpriteHeight;
+        public int SpriteWidth; // a single sprite width in pixels
+        public int SpriteHeight; // a single sprite height in pixels
 
-        public int Width;
-        public int Height;
+        public int Width; // sprite sheet width in pixels
+        public int Height; // sprite sheet height in pixels
     }
 }
