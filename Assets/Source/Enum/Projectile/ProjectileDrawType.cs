d58bbccda4d2afe8c661686ff45fb58faeec0fc5
Choi <ridvansu@hotmail.com>
2022-10-27 13:47:28 +0300

    Projectile Enums Refactor
    Projectile Enums Refactor.
    Issue #749

---
+++ Assets/Source/Enum/Projectile/ProjectileDrawType.cs
@@ -0,0 +1,10 @@
+namespace Enums
+{
+    //Note: can probably be deleted
+    //But dont delete, because might use later
+    public enum ProjectileDrawType
+    {
+        Invalid = 0,
+        Standard
+    }
+}
