5340287ab8df3fbe9d6ebf998967804b8e554c3f
unknown <mahlledz@gmail.com>
2022-11-02 13:04:36 +0100

    tile geometry enum fix

---
+++ Assets/Source/Collisions/Enums/TileGeometry.cs
@@ -0,0 +1,16 @@
+namespace Collisions
+{
+    public enum TileGeometry
+    {
+        Error = 0,
+        SB,
+        HB,
+        TB,
+        L1,
+        L2,
+        QP,
+        HP,
+        FP
+    }
+}
+
