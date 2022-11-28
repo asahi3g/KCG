b155305c8c9fa9322e9006060bc3dae66b808dc9
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2022-11-28 11:08:15 +0100

    some comments

--- Assets/_Karl/Scripts/Container/Container.cs
+++ Assets/_Karl/Scripts/Container/Container.cs
@@ -1,5 +1,13 @@
 using UnityEngine.Events;
 
+
+// Note(Brandon): we should be using a struct instead of a class for Agent Stats
+// right now this code is using a class for each agent stat (Health, Food ...)
+// --- 
+// The Gui should use GetStat to determine if the value has changed instead of onChange Event
+// ---
+// We Should have an enum for each Stat Type (Health, Food ...)
+
 [System.Serializable]
 public abstract class Container<T>
 {
